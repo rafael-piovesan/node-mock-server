f168e93452913240dca940078f4473da9fcaef82
Rafael Piovesan <rubens.piovesan@gmail.com>
2022-11-30 14:08:11 -0300

    feat: take routes file as arg

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-const routes = require('./routes.json')
-const express = require('express')
-const morgan = require('morgan')
-const proxy = require('express-http-proxy')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const proxy = require("express-http-proxy");
+const app = express();
+const port = 3000;
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-app.use(morgan('combined'))
-
-Object.keys(routes).forEach(k => {
-    ({ forwardTo } = routes[k])
-
-    if (forwardTo) {
-        ({ path } = forwardTo)
-        app.use(k, proxy(forwardTo.host, {
-            proxyReqPathResolver: function (_) {                
-                return path
-              }
-        }))
-        return
+let routes = require('./routes.json');
+if (process.argv.slice(2)?.length > 0) {
+    const path = process.argv[2];
+    try {
+        routes = require(path);
+    } catch(e) {
+        console.log(`Could not load routes from file [${path}]`, e);
+        return;
     }
+}
 
-    app.all(k, async (_, res) => {
-        ({ delay, statusCode, body } = routes[k])
-        statusCode = statusCode ?? 200
-        body = body ?? 'empty body'
-        if (delay) {
-            await sleep(delay)
-        }
-        res.status(statusCode).send(body)
-    })
+app.use(morgan("combined"));
 
-})
+Object.keys(routes).forEach((k) => {
+  ({ forwardTo, method = "get" } = routes[k]);
+
+  if (forwardTo) {
+    ({ path } = forwardTo);
+    app.use(
+      k,
+      proxy(forwardTo.host, {
+        proxyReqPathResolver: function (_) {
+          return path;
+        },
+      })
+    );
+    return;
+  }
+
+  app[method.toLowerCase()](k, async (_, res) => {
+    ({ delay, statusCode, body } = routes[k]);
+    statusCode = statusCode ?? 200;
+    body = body ?? "empty body";
+    if (delay) {
+      await sleep(delay);
+    }
+    res.status(statusCode).set("Access-Control-Allow-Origin", "*").send(body);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
